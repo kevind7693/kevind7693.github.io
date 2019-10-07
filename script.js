7f16882a1a22098b34f6253e57e135d1d3c5e3ec
kevind7693 <44926820+kevind7693@users.noreply.github.com>
2019-10-07 10:27:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Kevin Dong"
+let userName = "kevind7693";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
